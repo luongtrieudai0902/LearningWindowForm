8bd9a19a18c1cfad7ab62d9119282bed898a5d22
Luong Trieu Dai <52300185@student.tdtu.edu.vn>
2025-01-20 08:08:21 +0700

    Cập nhật .gitignore

--- obj/Debug/net8.0-windows/LearningWinform.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/LearningWinform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningWinform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eebc3c946a157ac575288d09ce9960da56ea7d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningWinform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningWinform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
